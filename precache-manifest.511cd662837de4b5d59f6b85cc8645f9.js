0dc09fe21449858b9758394dcadd6dec5a3f8a27
Hrithik Raj <hraj2661999@gmail.com>
2020-10-28 03:00:44 +0530

    Updates

---
+++ precache-manifest.511cd662837de4b5d59f6b85cc8645f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027b5c25ad693f013b48c3a20b7ea67b",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "678174b551062f614f21",
+    "url": "/Recipe-App/static/css/main.bce79d2f.chunk.css"
+  },
+  {
+    "revision": "561627dd43f984707bc0",
+    "url": "/Recipe-App/static/js/2.2cc238f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-App/static/js/2.2cc238f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678174b551062f614f21",
+    "url": "/Recipe-App/static/js/main.1e74a28b.chunk.js"
+  },
+  {
+    "revision": "c035ec1896e8fb26f1a6",
+    "url": "/Recipe-App/static/js/runtime-main.5105d1b0.js"
+  }
+]);
